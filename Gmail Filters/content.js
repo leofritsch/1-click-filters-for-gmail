ff3436cad3743cfb94b6b7c16c836429b98ae811
Tan Shin Jie <shinjie.work@gmail.com>
2024-11-05 11:11:27 +0800

    refactor: dedup "runMyScript" from background.js

--- Gmail Filters/content.js
+++ Gmail Filters/content.js
@@ -1,92 +1,185 @@
+console.log("Content script initialised.");
 function runMyScript() {
+  function removeDuplicates(arr) {
+    let set = new Set();
 
+    // Adding elements to LinkedHashSet
+    for (let i = 0; i < arr.length; i++) {
+      set.add(arr[i]);
+    }
 
+    return Array.from(set);
+  }
 
-    // COLLECT ALL SELECTED EMAILS FROM GMAIL INBOX
+  console.log("Running content script...");
 
-    let newArray = [];
+  // COLLECT ALL SELECTED EMAILS FROM GMAIL INBOX
 
-    document.querySelectorAll(`div[aria-checked="true"][role="checkbox"]`).forEach(e => {
-        newArray.push(e.parentNode.parentNode.querySelector(`td.yX.xY [email*="@"]`).getAttribute("email"))
-        // e.parentNode.parentNode.querySelector(`td.yX.xY [email*="@"]`).getAttribute("email")
-    })
+  let newArray = [];
 
-    console.log(newArray);
-    console.log(newArray.join(" OR "));
+  document
+    .querySelectorAll(`div[aria-checked="true"][role="checkbox"]`)
+    .forEach((e) => {
+      newArray.push(
+        e.parentNode.parentNode
+          .querySelector(`td.yX.xY [email*="@"]`)
+          .getAttribute("email")
+      );
+      // e.parentNode.parentNode.querySelector(`td.yX.xY [email*="@"]`).getAttribute("email")
+    });
 
+  newArray = removeDuplicates(newArray);
+
+  console.log(newArray);
+  console.log(newArray.join(" OR "));
+
+  try {
     //if new array is empty, then alert the user
     if (newArray.length == 0) {
-        alert("No emails selected. Please select emails and try again.")
-    }
-    else {
+      alert("No emails selected. Please select emails and try again.");
+    } else {
+      // CREATE NEW GMAIL FILTER
 
-        // CREATE NEW GMAIL FILTER
+      // OPEN GMAIL FILTERS PAGE
+      document
+        .querySelector(`button[aria-label="Advanced search options"]`)
+        .click();
 
-        // OPEN GMAIL FILTERS PAGE
-        document.querySelector(`button[aria-label="Advanced search options"]`).click()
+      // WAIT FOR THE PAGE TO LOAD
+      setTimeout(() => {
+        // ADD ALL COLLECTED EMAILS TO THE FILTER
+        // document.getElementById(`:pg`).value = newArray.join(" OR ")
+        document.querySelector(`.SK.ZF-zT > .ZZ .w-Pv input.ZH.nr.aQa`)
+          ? (document.querySelector(
+              `.SK.ZF-zT > .ZZ .w-Pv input.ZH.nr.aQa`
+            ).value = newArray.join(" OR "))
+          : (document.querySelector(
+              `.SK.ZF-zT > .ZZ .w-Pv input.ZH.nr.aQa`
+            )[1].value = newArray.join(" OR "));
+        // document.querySelector(`.SK.ZF-zT > .ZZ .w-Pv input.ZH.nr.aQa`).value = newArray.join(" OR ")
 
-        // WAIT FOR THE PAGE TO LOAD
+        newArray = [];
+
+        // WAIT FOR THE PAGE TO LOAD & CLICK ON CREATE FILTER BUTTON
         setTimeout(() => {
-            // ADD ALL COLLECTED EMAILS TO THE FILTER
-            // document.getElementById(`:pg`).value = newArray.join(" OR ")
-            document.querySelector(`.SK.ZF-zT > .ZZ .w-Pv input.ZH.nr.aQa`) ? document.querySelector(`.SK.ZF-zT > .ZZ .w-Pv input.ZH.nr.aQa`).value = newArray.join(" OR ") : document.querySelector(`.SK.ZF-zT > .ZZ .w-Pv input.ZH.nr.aQa`)[1].value = newArray.join(" OR ");
-            // document.querySelector(`.SK.ZF-zT > .ZZ .w-Pv input.ZH.nr.aQa`).value = newArray.join(" OR ")
+          document.querySelector(`.acM[role="link"]`).click();
 
-            
+          // PREFILL IMPORTANT PARAMETERS OF THE FILTER
 
-            // WAIT FOR THE PAGE TO LOAD & CLICK ON CREATE FILTER BUTTON
-            setTimeout(() => {
-                document.querySelector(`.acM[role="link"]`).click()
+          // wait for the page to load
+          setTimeout(() => {
+            // CHECK THE BOX TO APPLY THE FILTER TO EXISTING CONVERSATIONS
+            try {
+              document.querySelector(`.btl.ZQ input.btj`).click();
+            } catch (error) {
+              console.log(
+                "Unable to check the box to apply the filter to existing conversations"
+              );
+              newArray = [];
+              console.log("Cleaning up...");
+              console.log("Return early...");
+              return;
+            }
 
-                // PREFILL IMPORTANT PARAMETERS OF THE FILTER
+            // NEVER SEND TO SPAM
+            try {
+              let spamLabel = document
+                .evaluate(
+                  "//label[text()='Never send it to Spam']",
+                  document,
+                  null,
+                  XPathResult.UNORDERED_NODE_ITERATOR_TYPE,
+                  null
+                )
+                .iterateNext();
+              spamLabel.click();
+            } catch (error) {
+              console.log("No never send to spam option");
+              newArray = [];
+              console.log("Cleaning up...");
+              console.log("Return early...");
+              return;
+            }
 
-                // wait for the page to load
-                setTimeout(() => {
+            // SKIP THE INBOX
+            try {
+              let archiveLabel = document
+                .evaluate(
+                  "//label[contains(text(),'Skip the Inbox')]",
+                  document,
+                  null,
+                  XPathResult.UNORDERED_NODE_ITERATOR_TYPE,
+                  null
+                )
+                .iterateNext();
+              archiveLabel.click();
+            } catch (error) {
+              console.log(error);
+              console.log("Unable to check skip the inbox");
+              newArray = [];
+              console.log("Cleaning up...");
+              console.log("Return early...");
+              return;
+            }
 
-                    // CECK THE BOX TO APPLY THE FILTER TO EXISTING CONVERSATIONS
-                    document.querySelector(`.btl.ZQ input.btj`).click()
+            // APPLY THE LABEL
+            try {
+              let applyLabel = document
+                .evaluate(
+                  "//label[contains(text(),'Apply the label')]",
+                  document,
+                  null,
+                  XPathResult.UNORDERED_NODE_ITERATOR_TYPE,
+                  null
+                )
+                .iterateNext();
+              applyLabel.click();
+            } catch (error) {
+              console.log("Unable to apply the label");
+              newArray = [];
+              console.log("Cleaning up...");
+              console.log("Return early...");
+              return;
+            }
 
-                    // NEVER SEND TO SPAM
-                    document.querySelector(`div:nth-child(7).nH.lZ > label`).click()
+            // HIGHLIGHT THE LABEL SECTION SO USER CAN PICK
+            document.querySelector(
+              ".T-axO.T-I.T-I-ax7.J-J5-Ji.J-JN-M-I.ZE"
+            ).style.border = "solid 3px orange";
+            document.querySelector(
+              ".T-axO.T-I.T-I-ax7.J-J5-Ji.J-JN-M-I.ZE"
+            ).style.borderRadius = "7px";
+            document.querySelector(
+              ".T-axO.T-I.T-I-ax7.J-J5-Ji.J-JN-M-I.ZE"
+            ).style.padding = "3px";
 
-                    // SKIP THE INBOX
-                    document.querySelector(`div:nth-child(1).nH.lZ > label`).click()
-
-                    // APPLY THE LABEL
-                    document.querySelector(`div:nth-child(4).nH.lZ > label`).click()
-
-                    // HIGHLIGHT THE LABEL SECTION SO USER CAN PICK
-                    document.querySelector(".T-axO.T-I.T-I-ax7.J-J5-Ji.J-JN-M-I.ZE").style.border = "solid 3px orange";
-                    document.querySelector(".T-axO.T-I.T-I-ax7.J-J5-Ji.J-JN-M-I.ZE").style.borderRadius = "7px";
-                    document.querySelector(".T-axO.T-I.T-I-ax7.J-J5-Ji.J-JN-M-I.ZE").style.padding = "3px";
-
-                    // ADD AN ARROW POINTING TO THE LABEL SECTION
-                    document.querySelector(".T-axO.T-I.T-I-ax7.J-J5-Ji.J-JN-M-I.ZE > div").insertAdjacentHTML("afterend", `<div style="position: absolute; top: 0; right: -20px; width: 0; height: 0; border-top: 10px solid transparent; border-bottom: 10px solid transparent; border-right: 10px solid orange;"></div>`)
-                    // ADD SOME TEXT NEXT TO THE ARROW
-                    document.querySelector(".T-axO.T-I.T-I-ax7.J-J5-Ji.J-JN-M-I.ZE > div").insertAdjacentHTML("afterend", `<div style="position: absolute; top: 0; right: -50px; width: 0; height: 0; border-top: 0px solid transparent; border-bottom: 10px solid transparent; color:orange; border-right: 10px solid white;">Pick a label to keep organised (recommended)</div>`)
-
-                }, 1500)
-
-
-            }, 700)
-
-
-
-
-
-        }, 1000)
-
-
-
-
+            // ADD AN ARROW POINTING TO THE LABEL SECTION
+            document
+              .querySelector(".T-axO.T-I.T-I-ax7.J-J5-Ji.J-JN-M-I.ZE > div")
+              .insertAdjacentHTML(
+                "afterend",
+                `<div style="position: absolute; top: 0; right: -20px; width: 0; height: 0; border-top: 10px solid transparent; border-bottom: 10px solid transparent; border-right: 10px solid orange;"></div>`
+              );
+            // ADD SOME TEXT NEXT TO THE ARROW
+            document
+              .querySelector(".T-axO.T-I.T-I-ax7.J-J5-Ji.J-JN-M-I.ZE > div")
+              .insertAdjacentHTML(
+                "afterend",
+                `<div style="position: absolute; top: 0; right: -50px; width: 0; height: 0; border-top: 0px solid transparent; border-bottom: 10px solid transparent; color:orange; border-right: 10px solid white;">Pick a label to keep organised (recommended)</div>`
+              );
+          }, 1500);
+        }, 700);
+      }, 1000);
     }
+  } catch (error) {
+    newArray = [];
+    console.log("Cleaning up...");
+    throw error;
+  }
 }
-
-
-
 // Add this event listener at the end of content.js
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.toggle) {
-        runMyScript();
-    }
+  if (request.toggle) {
+    runMyScript();
+  }
 });
